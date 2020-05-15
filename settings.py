0fb28896cb75392aecd8919f3b382a02c56ba332
Adam Dawoodjee <adam.daveed@gmail.com>
2020-05-15 18:24:27 +0200

    re-added settings

---
+++ settings.py
@@ -0,0 +1,24 @@
+database_host = "localhost"
+database_name = "DBName"
+erpnext_host = "https://erp.host"
+erpnext_user = "api"
+erpnext_password = "PASSW123"
+cost_centre = "Main - MCB"
+debit_to = "0008/002 - Debtors - MCB"
+default_company = "Company Name"
+default_customer = "Default Customer"
+income_account = "0001/001 - Cash - MCB"
+restaurant_name = "No. 1"
+tax_rate = "16"
+territory = "Zambia"
+last_update = ""
+bank_payment_account = "0009/002 - First National Bank - MCB"
+cash_payment_account = "0009/006 - Cash Control - MCB"
+pos_profile = "Centro Mall"
+
+"""
+Add?
+- selling_warehouse
+- raw_material_warehouse
+
+"""
